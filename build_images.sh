5b07a067e5a49028f9dbbf0e0694419dfcc53a92
jwlodek <jwlodek@cs.stonybrook.edu>
2019-08-20 15:14:34 -0400

    Update run script, improved dockerfiles, added Ubuntu 19.04

--- build_images.sh
+++ build_images.sh
@@ -6,6 +6,10 @@ cd Ubuntu-18.04
 docker build -t isa/ubuntu18.04 .
 cd ..
 
+cd Ubuntu-19.04
+docker build -t isa/ubuntu19.04 .
+cd ..
+
 cd Debian-8
 docker build -t isa/debian8 .
 cd ..
---
+++ run_container.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+to_run=$1
+
+function run_container () {
+    container=$1
+    echo "Starting $container build"
+    docker run --cidfile "$container"_id.cid isa/"$container"
+    CONTAINER_ID=$(cat "$container"_id.cid)
+    rm "$container"_id.cid
+    echo "Copying package..."
+    docker cp $CONTAINER_ID:/installSynApps/DEPLOYMENTS $(pwd)/DEPLOYMENTS/.
+    mv DEPLOYMENTS/DEPLOYMENTS/* DEPLOYMENTS/.
+    rmdir DEPLOYMENTS/DEPLOYMENTS
+    echo "Shutting down container."""
+    docker container rm $CONTAINER_ID
+}
+
+if [ "$to_run" = "help" ];
+then
+echo "USAGE:"
+echo "  ./run_container.sh all - will run all docker containers sequentially."
+echo "  ./run_container OS - will run single container."
+echo
+echo "Supported containers: [ ubuntu18.04, ubuntu19.04, debian8, debian9 ]"
+echo
+exit
+fi
+
+if [ "$to_run" = "all" ];
+then
+run_container ubuntu18.04
+run_container ubuntu19.04
+run_container debian8
+run_container debian9
+else
+run_container "$to_run"
+fi
+
+
+echo "Build done. Bundles placed in ./DEPLOYMENTS"
+exit
