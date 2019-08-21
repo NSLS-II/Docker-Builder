d29197337f2594b17621e20fe15945f85990b411
jwlodek <jwlodek@cs.stonybrook.edu>
2019-08-21 14:57:10 -0400

    Use installSynApps master branch

--- Debian-9/run_build.sh
+++ Debian-9/run_build.sh
@@ -3,7 +3,7 @@
 # This bash script will be run with when the docker image generated from the
 # Dockerfile in this directory is run
 
-git clone --single-branch --branch=feature-custom-build-scripts https://github.com/jwlodek/installSynApps
+git clone https://github.com/epicsNSLS2-deploy/installSynApps
 cd installSynApps
 python3 installCLI.py -y -c addtlConfDirs/configureDeb9 -i /epics
 
